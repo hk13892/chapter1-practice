d9438838c4344454103f08cb5120f5c50c27b1ca
hk13892 <sr1729@trinity.col>
2019-04-19 22:04:10 +0530

    a->x and 3 to 4

--- src/square.js
+++ src/square.js
@@ -1,9 +1,9 @@
-function square(a){
-    return a*a;
+function square(x){
+    return x*x;
 }
 // I just added a line to print the result
 console.log(square(5));
 
-square(5);
+
 //Added line to print square root of 3
-console.log(square(3));
+console.log(square(4));
