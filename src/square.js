213ce6b13f7549e1f76373d5c43225d508aea5d9
hk13892 <49691194+hk13892@users.noreply.github.com>
2019-04-19 22:06:38 +0530

    a->y & 5 to 4

--- src/square.js
+++ src/square.js
@@ -1,8 +1,8 @@
-function square(a){
-    return a*a;
+function square(y){
+    return y*y;
 }
 // I just added a line to print the result
-console.log(square(5));
+console.log(square(4));
 
 square(5);
 //Added line to print square root of 3
